b9288b88b0dbe46478240b9b21f6e40984fe3b6b
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-03 01:27:39 +0900

    Component, props, state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import MyComponent from './MyComponent';
+import Say from './Say';
 
 const App = () => {
-  return (
-    <MyComponent name="React" favoriteNumber={1}>
-      리액트
-    </MyComponent>
-  );
+  return <Say />;
 };
 
 export default App;
