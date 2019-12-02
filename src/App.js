f62031ec4cc38bfd944d94141beaa01c0f3dac39
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-03 00:09:59 +0900

    함수형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    const name = 'React';
-    return <div className="react">{name}</div>;
-  }
+function App() {
+  const name = 'React';
+  return <div className="react">{name}</div>;
 }
 
 export default App;
