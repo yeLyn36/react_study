d98924b461bf549918038996c1c0308b39b91896
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-03 00:09:39 +0900

    클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  const name = "React";
-  return <div className="react">{name}</div>;
+class App extends Component {
+  render() {
+    const name = 'React';
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
