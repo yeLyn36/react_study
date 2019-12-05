6dde00b218bbcc26388f57ffd09681be62c289c6
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-05 22:48:52 +0900

    study css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
-
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+import logo from './logo.svg';
 
 class App extends Component {
-  state = {
-    color: '#000000'
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  };
-
   render() {
     return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
+      <div className="App">
+        <header>
+          <img src={logo} className="logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn react
+          </a>
+        </header>
       </div>
     );
   }
