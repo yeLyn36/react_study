4da0532c3447d2aeefb5ba0fbc6bfa8c97bdacf4
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-05 22:18:43 +0900

    add files using Hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
