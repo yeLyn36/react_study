28fe541e5584cb49149446c4a649130d9bf9dbdf
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-14 23:55:38 +0900

    create  context API

---
+++ src/context/ColorBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ColorContext from './color';
+
+const ColorBox = () => {
+  return (
+    <ColorContext.Consumer>
+      {value => (
+        <div
+          style={{
+            width: '64px',
+            height: '64px',
+            background: value.color,
+          }}
+        />
+      )}
+    </ColorContext.Consumer>
+  );
+};
+
+export default ColorBox;
