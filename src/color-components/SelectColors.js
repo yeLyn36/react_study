8b5c2a33398aa0d3097446051e394b45caa87d06
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-18 22:19:14 +0900

    use Consumer and Provider

--- src/color-components/SelectColors.js
+++ src/color-components/SelectColors.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ColorConsumer } from '../context/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
@@ -6,19 +7,28 @@ const SelectColors = () => {
   return (
     <div>
       <h2>색상을 선택하세요.</h2>
-      <div style={{ display: 'flex' }}>
-        {colors.map(color => (
-          <div
-            key={color}
-            style={{
-              background: color,
-              width: '24px',
-              height: '24px',
-              cursor: 'pointer',
-            }}
-          />
-        ))}
-      </div>
+      <ColorConsumer>
+        {({ actions }) => (
+          <div style={{ display: 'flex' }}>
+            {colors.map(color => (
+              <div
+                key={color}
+                style={{
+                  background: color,
+                  width: '24px',
+                  height: '24px',
+                  cursor: 'pointer',
+                }}
+                onClick={() => actions.setColor(color)}
+                onContextMenu={e => {
+                  e.preventDefault();
+                  actions.setSubcolor(color);
+                }}
+              />
+            ))}
+          </div>
+        )}
+      </ColorConsumer>
       <hr />
     </div>
   );
