589b345f30bb8c1afd0eeabc0036158fe18ad8b2
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-26 23:51:44 +0900

    add render

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -48,3 +48,15 @@ function reducer(state = initialState, action) {
 }
 
 const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState(); //현재 상태를 불러옴
+  //토글 처리
+  if (state.toggle) {
+    divToggle.classList.add('active');
+  } else {
+    divToggle.classList.remove('active');
+  }
+  //카운터 처리
+  counter.innerText = state.counter;
+};
