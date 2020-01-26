231ee8e47a68ecb6356923c70ce4df0ed73f2998
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-26 23:48:09 +0900

    add action

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,4 +1,46 @@
+//DOM 추가
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
+
+//액션 명
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+//액션 추가
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = difference => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+//초기값 설정
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+//state가 undefined일 떄는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  //action에 따라 다른 작업을 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        //상태 불변성 유지
+        ...state,
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
