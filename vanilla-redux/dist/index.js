83a35d46bc1db109e1f480694923f7d0771714b5
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-29 03:11:59 +0900

    run action

--- vanilla-redux/dist/index.js
+++ vanilla-redux/dist/index.js
@@ -986,7 +986,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63783" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63504" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -1163,4 +1163,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../Users/ghddn/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/vanilla-redux.e31bb0bc.js.map
+//# sourceMappingURL=/index.js.map
