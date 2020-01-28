2261592599db8c01d420b783dbb575f8ad53a026
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-29 03:11:32 +0900

    add redux-tutorial

---
+++ react-redux/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div>
+      <Counter number={0}></Counter>
+    </div>
+  );
+}
+
+export default App;
